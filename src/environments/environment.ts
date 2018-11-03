4368795270eb1d97937bd6fabc4659358c056ea8
zircon63 <canek1702@gmail.com>
2018-11-03 13:28:31 +0300

    build(dotenv): setup environments variables for spa

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    ENCRYPT_AES: 'l0v3r-S+uD10-h4X0r1',
-    ENCRYPT_HASH: '8zgqvU6LaziThJI1uz3PevYd',
-    MIND_API_URL: 'https://beta.api.mind.com',
-    MIND_APP_ID: '3392f844-9dbc-4251-b0ae-8a44efae95d1',
-    PRODUCTION: false,
-    SOCKET_URL: 'https://ucalls.ru:3443/spikaenterprise',
-    WEB_RTC_URL: 'https://ucalls.ru:3443/signaling',
-    };
