45606ba04bbe43d7bc5b9325f51bbc2bcfa9c9c5
zircon63 <canek1702@gmail.com>
2018-11-28 19:14:44 +0300

    feat(db): copy in appdata

--- electron.main.ts
+++ electron.main.ts
@@ -41,6 +41,7 @@ function createWindow() {
         electron: require(`${__dirname}/node_modules/electron`)
       });
       win.loadURL('http://localhost:4200');
+      win.webContents.openDevTools();
     } else {
       win.loadURL(url.format({
         pathname: path.join(__dirname, 'dist/index.html'),
@@ -48,7 +49,7 @@ function createWindow() {
         slashes: true
       }));
     }
-    win.webContents.openDevTools();
+
 
 
     // Emitted when the window is closed.
